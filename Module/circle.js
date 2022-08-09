b27c36e496ce4aec77914056a29ecf5088208e4e
Hüseyin Soylu <89314419+huseyinsoyluu@users.noreply.github.com>
2022-08-09 14:06:34 +0300

    Create circle.js

---
+++ Module/circle.js
@@ -0,0 +1,12 @@
+const circleArea = (radius) => {
+  console.log(Math.PI * radius ** 2);
+};
+
+const circleCircumference = (radius) => {
+  console.log(Math.PI * radius * 2);
+};
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
